bcd155ec57e5147e34f76c6ed63d1ad0251105e8
TranQuocTrinh <tranquoctrinh.ai@gmail.com>
2022-03-23 16:24:13 +0000

    fix

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import torch
+
 configs = {
     "train_source_data":"./data_en_vi/train.en",
     "train_target_data":"./data_en_vi/train.vi",
@@ -8,7 +10,7 @@ configs = {
     "source_max_seq_len":256,
     "target_max_seq_len":256,
     "batch_size":40,
-    "device":"cuda:0",
+    "device":"cuda:0" if torch.cuda.is_available() else "cpu",
     "embedding_dim": 512,
     "n_layers": 6,
     "n_heads": 8,
