2d985a0a4c4373c3ecd6969401cb07bf2716b2f2
TranQuocTrinh <tranquoctrinh.ai@gmail.com>
2022-03-24 04:59:20 +0000

    fix translate decoder step, and final flow

--- models.py
+++ models.py
@@ -1,122 +1,236 @@
 import torch
-from torch.utils.data import Dataset
-from transformers import AutoTokenizer
-import re
+import torch.nn as nn
+import numpy as np
+import math
+from torch.autograd import Variable
 
-class TranslateDataset(Dataset):
-    def __init__(self, source_tokenizer, target_tokenizer, source_data=None, target_data=None, source_max_seq_len=256, target_max_seq_len=256, phase="train"):
-        self.source_data = source_data
-        self.target_data = target_data
-        self.source_tokenizer = source_tokenizer
-        self.target_tokenizer = target_tokenizer
+# Embedding the input sequence
+class Embedding(nn.Module):
+    def __init__(self, vocab_size, embedding_dim):
+        super(Embedding, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+
+    def forward(self, x):
+        return self.embedding(x)
+
+# The positional encoding vector
+class PositionalEncoder(nn.Module):
+    def __init__(self, embedding_dim, max_seq_length=512, dropout=0.1):
+        super(PositionalEncoder, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.dropout = nn.Dropout(dropout)
+        pe = torch.zeros(max_seq_length, embedding_dim)
+        for pos in range(max_seq_length):
+            for i in range(0, embedding_dim, 2):
+                pe[pos, i] = math.sin(pos/(10000**(2*i/embedding_dim)))
+                pe[pos, i+1] = math.cos(pos/(10000**((2*i+1)/embedding_dim)))
+        pe = pe.unsqueeze(0)        
+        self.register_buffer('pe', pe)
+    
+    def forward(self, x):
+        x = x*math.sqrt(self.embedding_dim)
+        seq_length = x.size(1)
+        pe = Variable(self.pe[:, :seq_length], requires_grad=False).to(x.device)
+        # Add the positional encoding vector to the embedding vector
+        x = x + pe
+        x = self.dropout(x)
+        return x
+
+# Self-attention layer
+class SelfAttention(nn.Module):
+    ''' Scaled Dot-Product Attention '''
+
+    def __init__(self, dropout=0.1):
+        super(SelfAttention, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, query, key, value, mask=None):
+        key_dim = key.size(-1)
+        attn = torch.matmul(query / np.sqrt(key_dim), key.transpose(2, 3))
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+            attn = attn.masked_fill(mask == 0, -1e9)
+        attn = self.dropout(torch.softmax(attn, dim=-1))
+        output = torch.matmul(attn, value)
+
+        return output
+        
+# Multi-head attention layer
+class MultiHeadAttention(nn.Module):
+    def __init__(self, embedding_dim, num_heads, dropout=0.1):
+        super(MultiHeadAttention, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.self_attention = SelfAttention(dropout)
+        # The number of heads
+        self.num_heads = num_heads
+        # The dimension of each head
+        self.dim_per_head = embedding_dim // num_heads
+        # The linear projections
+        self.query_projection = nn.Linear(embedding_dim, embedding_dim)
+        self.key_projection = nn.Linear(embedding_dim, embedding_dim)
+        self.value_projection = nn.Linear(embedding_dim, embedding_dim)
+        self.dropout = nn.Dropout(dropout)
+        self.out = nn.Linear(embedding_dim, embedding_dim)
+
+    def forward(self, query, key, value, mask=None):
+        # Apply the linear projections
+        batch_size = query.size(0)
+        query = self.query_projection(query)
+        key = self.key_projection(key)
+        value = self.value_projection(value)
+        # Reshape the input
+        query = query.view(batch_size, -1, self.num_heads, self.dim_per_head).transpose(1, 2)
+        key = key.view(batch_size, -1, self.num_heads, self.dim_per_head).transpose(1, 2)
+        value = value.view(batch_size, -1, self.num_heads, self.dim_per_head).transpose(1, 2)
+        # Calculate the attention
+        scores = self.self_attention(query, key, value, mask)
+        # Reshape the output
+        output = scores.transpose(1, 2).contiguous().view(batch_size, -1, self.embedding_dim)
+        # Apply the linear projection
+        output = self.out(output)
+        return output
+
+# Norm layer
+class Norm(nn.Module):
+    def __init__(self, embedding_dim):
+        super(Norm, self).__init__()
+        self.norm = nn.LayerNorm(embedding_dim)
+
+    def forward(self, x):
+        return self.norm(x)
+
+
+# Transformer encoder layer
+class EncoderLayer(nn.Module):
+    def __init__(self, embedding_dim, num_heads, dropout=0.1):
+        super(EncoderLayer, self).__init__()
+        self.self_attention = MultiHeadAttention(embedding_dim, num_heads, dropout)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embedding_dim, embedding_dim * 4),
+            nn.ReLU(),
+            nn.Linear(embedding_dim * 4, embedding_dim)
+        )
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.norm1 = Norm(embedding_dim)
+        self.norm2 = Norm(embedding_dim)
+
+    def forward(self, x, mask=None):
+        x2 = self.norm1(x)
+        # Add and Muti-head attention
+        x = x + self.dropout1(self.self_attention(x2, x2, x2, mask))
+        x2 = self.norm2(x)
+        x = x + self.dropout2(self.feed_forward(x2))
+        return x
+
+# Transformer decoder layer
+class DecoderLayer(nn.Module):
+    def __init__(self, embedding_dim, num_heads, dropout=0.1):
+        super(DecoderLayer, self).__init__()
+        self.self_attention = MultiHeadAttention(embedding_dim, num_heads, dropout)
+        self.encoder_attention = MultiHeadAttention(embedding_dim, num_heads, dropout)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embedding_dim, embedding_dim * 4),
+            nn.ReLU(),
+            nn.Linear(embedding_dim * 4, embedding_dim)
+        )
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.dropout3 = nn.Dropout(dropout)
+        self.norm1 = Norm(embedding_dim)
+        self.norm2 = Norm(embedding_dim)
+        self.norm3 = Norm(embedding_dim)
+
+    def forward(self, x, memory, source_mask, target_mask):
+        x2 = self.norm1(x)
+        x = x + self.dropout1(self.self_attention(x2, x2, x2, target_mask))
+        x2 = self.norm2(x)
+        x = x + self.dropout2(self.self_attention(x2, memory, memory, source_mask))
+        x2 = self.norm3(x)
+        x = x + self.dropout3(self.feed_forward(x2))
+        return x
+
+# Encoder transformer
+class Encoder(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, max_seq_len, num_heads, num_layers, dropout=0.1):
+        super(Encoder, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.num_layers = num_layers
+        self.num_heads = num_heads
+        self.embedding_dim = embedding_dim
+        self.layers = nn.ModuleList([EncoderLayer(embedding_dim, num_heads, dropout) for _ in range(num_layers)])
+        self.norm = Norm(embedding_dim)
+        self.position_embedding = PositionalEncoder(embedding_dim, max_seq_len, dropout)
+    
+    def forward(self, source, source_mask):
+        # Embed the source
+        x = self.embedding(source)
+        # Add the position embeddings
+        x = self.position_embedding(x)
+        # Propagate through the layers
+        for layer in self.layers:
+            x = layer(x, source_mask)
+        # Normalize
+        x = self.norm(x)
+        return x
+
+# Decoder transformer
+class Decoder(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, max_seq_len,num_heads, num_layers, dropout=0.1):
+        super(Decoder, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.num_layers = num_layers
+        self.num_heads = num_heads
+        self.embedding_dim = embedding_dim
+        self.layers = nn.ModuleList([DecoderLayer(embedding_dim, num_heads, dropout) for _ in range(num_layers)])
+        self.norm = Norm(embedding_dim)
+        self.position_embedding = PositionalEncoder(embedding_dim, max_seq_len, dropout)
+    
+    def forward(self, target, memory, source_mask, target_mask):
+        # Embed the source
+        x = self.embedding(target)
+        # Add the position embeddings
+        x = self.position_embedding(x)
+        # Propagate through the layers
+        for layer in self.layers:
+            x = layer(x, memory, source_mask, target_mask)
+        # Normalize
+        x = self.norm(x)
+        return x
+
+
+# Transformers
+class Transformer(nn.Module):
+    def __init__(self, source_vocab_size, target_vocab_size, source_max_seq_len, target_max_seq_len, embedding_dim, num_heads, num_layers, dropout=0.1):
+        super(Transformer, self).__init__()
+        self.source_vocab_size = source_vocab_size
+        self.target_vocab_size = target_vocab_size
         self.source_max_seq_len = source_max_seq_len
         self.target_max_seq_len = target_max_seq_len
-        self.phase = phase
+        self.embedding_dim = embedding_dim
+        self.num_heads = num_heads
+        self.num_layers = num_layers
+        self.dropout = dropout
     
-    def preprocess_seq(self, seq):
-        seq = re.sub(
-        r"[\*\"“”\n\\…\+\-\/\=\(\)‘•:\[\]\|’\!;]", " ", str(seq))
-        seq = re.sub(r"[ ]+", " ", seq)
-        seq = re.sub(r"\!+", "!", seq)
-        seq = re.sub(r"\,+", ",", seq)
-        seq = re.sub(r"\?+", "?", seq)
-        seq = seq.lower()
-        return seq
-
-    def convert_line_uncased(self, tokenizer, text, max_seq_len):
-        tokens = tokenizer.tokenize(text)[:max_seq_len-2]
-        tokens = [tokenizer.cls_token] + tokens + [tokenizer.sep_token]
-        tokens += [tokenizer.pad_token]*(max_seq_len - len(tokens))
-        token_idx = tokenizer.convert_tokens_to_ids(tokens)
-        return tokens, token_idx
+        self.encoder = Encoder(source_vocab_size, embedding_dim, source_max_seq_len, num_heads, num_layers, dropout)
+        self.decoder = Decoder(target_vocab_size, embedding_dim, target_max_seq_len, num_heads, num_layers, dropout)
+        self.final_linear = nn.Linear(embedding_dim, target_vocab_size)
+        self.dropout = nn.Dropout(dropout)
     
-    def __len__(self):
-        return len(self.source_data)
-
-    # create decoder input mask
-    def create_decoder_mask(self, seq_len):
-        mask = torch.ones(seq_len, seq_len).tril()
-        return mask
+    def forward(self, source, target, source_mask, target_mask):
+        # Encoder forward pass
+        memory = self.encoder(source, source_mask)
+        # Decoder forward pass
+        output = self.decoder(target, memory, source_mask, target_mask)
+        # Final linear layer
+        output = self.dropout(output)
+        output = self.final_linear(output)
+        return output
     
-    def __getitem__(self, index):
-        # source_seq, source_idx = self.convert_line_uncased(
-        #     tokenizer=self.source_tokenizer, 
-        #     text=self.preprocess_seq(self.source_data[index]), 
-        #     max_seq_len=self.source_max_seq_len
-        # )
-        # target_seq, target_idx = self.convert_line_uncased(
-        #     tokenizer=self.target_tokenizer, 
-        #     text=self.preprocess_seq(self.target_data[index]), 
-        #     max_seq_len=self.target_max_seq_len
-        # )
-        
-        source = self.source_tokenizer(
-                text=self.preprocess_seq(self.source_data[index]),
-                padding="max_length", 
-                max_length=self.source_max_seq_len, 
-                truncation=True, 
-                return_tensors="pt"
-            )
-        
-        if self.phase == "train":
-            target = self.target_tokenizer(
-                text=self.preprocess_seq(self.target_data[index]),
-                padding="max_length",
-                max_length=self.target_max_seq_len,
-                truncation=True,
-                return_tensors="pt"
-            )
+    def make_source_mask(self, source_ids, source_pad_id):
+        return (source_ids != source_pad_id).unsqueeze(-2)
 
-            return {
-                "source_seq": self.source_data[index],
-                "source_ids": source["input_ids"][0],
-                "target_seq": self.target_data[index],
-                "target_ids": target["input_ids"][0],
-                }
-        else:
-            return {
-                "source_seq": self.source_data[index],
-                "source_ids": source["input_ids"][0],
-            }
-
-
-def main():
-    from utils import configs
-
-    def read_data(source_file, target_file):
-        source_data = open(source_file).read().strip().split("\n")
-        target_data = open(target_file).read().strip().split("\n")
-        return source_data, target_data
-
-    train_src_data, train_trg_data = read_data(configs["train_source_data"], configs["train_target_data"])
-    valid_src_data, valid_trg_data = read_data(configs["valid_source_data"], configs["valid_target_data"])
-
-    source_tokenizer = AutoTokenizer.from_pretrained(configs["source_tokenizer"])
-    target_tokenizer = AutoTokenizer.from_pretrained(configs["target_tokenizer"])
-    train_dataset = TranslateDataset(
-        source_tokenizer=source_tokenizer, 
-        target_tokenizer=target_tokenizer, 
-        source_data=train_src_data, 
-        target_data=train_trg_data, 
-        source_max_seq_len=configs["source_max_seq_len"],
-        target_max_seq_len=configs["target_max_seq_len"]
-    )
-    print(train_dataset[0])
-
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=configs["batch_size"],
-        shuffle=True
-    )
-    for batch in train_loader:
-        print(batch["source_seq"])
-        print(batch["target_seq"])
-        print(batch["source_ids"])
-        print(batch["target_ids"])
-        break
-
-    import ipdb; ipdb.set_trace()
-
-
-if __name__ == "__main__":
-    main()
+    def make_target_mask(self, target_ids):
+        batch_size, len_target = target_ids.size()
+        subsequent_mask = (1 - torch.triu(torch.ones((1, len_target, len_target), device=target_ids.device), diagonal=1)).bool()
+        return subsequent_mask
